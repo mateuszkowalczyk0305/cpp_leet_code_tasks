c5e8a2af8cc4c48929496fdcc8e39e8aff58d088
Mateusz <mateusz.kowalczyk.0305@gmail.com>
2025-04-06 23:20:47 +0200

    Task COntent: Plus One [easy] added

---
+++ PlusOne[easy]/plusOne.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> plusOne(std::vector<int> &digits) {}
+
+int main() {
+  std::vector<int> a = {1, 2, 3};
+  std::vector<int> c = {4, 3, 2, 1};
+  std::vector<int> b = {9};
+  return 0;
+}
